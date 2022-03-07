f6e388ba6dfb19ce412f3d31f0330122b1682493
alx3dev <alx3dev@gmail.com>
2022-03-07 14:51:43 +0100

    edit doc

--- lib/exer.rb
+++ lib/exer.rb
@@ -9,6 +9,8 @@ require_relative 'exer/make'
 # Go language is bundled together within the app,
 # so all you need is linux and ruby.
 #
+# @see Exer::Make
+#
 # @example Build executable installers for glimmer gem
 #
 #  Exer.make('glimmer-installer') do |app|
@@ -16,10 +18,27 @@ require_relative 'exer/make'
 #  end
 #
 module Exer
-  def self.make(opts = {})
-    maker = Exer::Make.new(opts[:filename])
+  # Class method #make is a shortcut to initialize new Maker instance,
+  # and add default functions.
+  #
+  # @see Exer::Make
+  #
+  # @example Build Windows installer for gem 'my_gem'
+  #
+  #   Exer.make(exclude: [:windows, :darwin]) do |build|
+  #     build.filename = 'my_gem'
+  #     build.add :gem_install, 'my_gem'
+  #   end
+  #
+  # @param [Hash] options Optional.
+  # @option options [String] filename Optional. Filename, defined here or inside a block.
+  # @option options [Symbol] exclude Optional. Platforms to exclude when making binaries.
+  # @yield [Exer::Make] New instance of make class, with added default functions.
+  #
+  def self.make(options = {})
+    maker = Exer::Make.new(options[:filename])
     maker.add_defaults
     yield(maker) if block_given?
-    maker.build opts[:exclude]
+    maker.build options[:exclude]
   end
 end
